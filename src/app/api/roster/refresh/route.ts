7f96b3fae125788e32cf8a6586e0ec1dc8396d5c
intern1001optical <intern1001optical@gmail.com>
2025-10-14 10:30:28 +1100

    router endpoint add a range params

--- src/app/api/roster/refresh/route.ts
+++ src/app/api/roster/refresh/route.ts
@@ -2,23 +2,41 @@ import { NextResponse } from "next/server";
 import {I1001Response} from "@/types/api_response";
 import {optomData} from "@/types/types";
 import {getEmploymentHeroList} from "@/lib/getEmploymentHeroList";
+import {toLocalIsoNoOffset} from "@/utils/time";
+import {startOfDay, endOfDay, endOfMonth, addMonths} from "date-fns";
 
 
 export async function GET(request: Request): Promise<NextResponse<I1001Response<optomData[]>>>  {
     try {
         // 쿼리 파라미터 읽기
         const { searchParams } = new URL(request.url);
-        const fromDate = searchParams.get("from");
-        const toDate = searchParams.get("to");
+        let fromDate = searchParams.get("from") ?? "";
+        let toDate = searchParams.get("to") ?? "";
+        const range = searchParams.get("range");
 
-        if(!fromDate || !toDate) {
-            console.error("Missing required parameters: from and to dates");
-            return NextResponse.json(
-                {
-                    message: "Missing required parameters: from and to dates",
-                },
-                { status: 400 }
-            );
+        if(range){
+            switch(range){
+                case "today":
+                    const todayDate = new Date();
+                    fromDate = toLocalIsoNoOffset(startOfDay(todayDate));
+                    toDate = toLocalIsoNoOffset(endOfDay(todayDate));
+                    break;
+                case "monthly":
+                    const currentDate = new Date();
+                    fromDate = toLocalIsoNoOffset(startOfDay(currentDate));
+                    toDate = toLocalIsoNoOffset(endOfMonth(addMonths(currentDate, 1)));
+                    break;
+            }
+        }else{
+            if(!fromDate || !toDate){
+                console.error("Missing required parameters: from and to dates OR range");
+                return NextResponse.json(
+                    {
+                        message: "Missing required parameters: from and to dates OR range",
+                    },
+                    { status: 400 }
+                );
+            }
         }
 
         // 날짜 형식 검증
